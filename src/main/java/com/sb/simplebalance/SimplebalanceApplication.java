39ef1c9520fd556feaf2657df40e5fbd239caf42
minseoBae <baejjae8218@gmail.com>
2025-01-12 17:30:30 +0900

    프ëfirst commit with gitignore

---
+++ src/main/java/com/sb/simplebalance/SimplebalanceApplication.java
@@ -0,0 +1,13 @@
+package com.sb.simplebalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplebalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplebalanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/simplebalance/SimplebalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.simplebalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplebalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
